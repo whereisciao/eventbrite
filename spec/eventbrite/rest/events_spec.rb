dba67b90c35bcce02a217ca2ddb5b9525653dfb1
Steven Chau <steven.p.chau@gmail.com>
2014-04-23 12:45:47 -0700

    Adjust spec formatting

--- spec/eventbrite/rest/events_spec.rb
+++ spec/eventbrite/rest/events_spec.rb
@@ -4,13 +4,17 @@ describe Eventbrite::REST::Events do
   let(:client) { Eventbrite::REST::Client.new(oauth_token:"TOKEN") }
 
   describe '.event_details' do
-    subject { client.event_details('10851193211') }
+    subject { client.event_details('123456789') }
 
     before do
-      stub_get('/v3/events/10851193211/').to_return(:body => fixture('event_details.json'), :headers => {:content_type => 'application/json; charset=utf-8'})
+      stub_get('/v3/events/123456789/').
+        to_return(
+          :body => fixture('event_details.json'),
+          :headers => {:content_type => 'application/json; charset=utf-8'}
+        )
     end
 
-    it { subject; a_get('/v3/events/10851193211/').should have_been_made }
+    it { subject; a_get('/v3/events/123456789/').should have_been_made }
     it { should be_a_kind_of(Eventbrite::Event) }
   end
 
