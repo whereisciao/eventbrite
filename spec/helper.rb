e2756d107679a43104274110c454b0b3da73bcd3
Steven Chau <steven.p.chau@gmail.com>
2014-04-23 12:14:19 -0700

    import stub request helpers

--- spec/helper.rb
+++ spec/helper.rb
@@ -12,3 +12,43 @@ require 'eventbrite'
 require 'rspec'
 require 'webmock/rspec'
 require 'pry'
+
+def a_delete(path)
+  a_request(:delete, Eventbrite::REST::Client::ENDPOINT + path)
+end
+
+def a_get(path)
+  a_request(:get, Eventbrite::REST::Client::ENDPOINT + path)
+end
+
+def a_post(path)
+  a_request(:post, Eventbrite::REST::Client::ENDPOINT + path)
+end
+
+def a_put(path)
+  a_request(:put, Eventbrite::REST::Client::ENDPOINT + path)
+end
+
+def stub_delete(path)
+  stub_request(:delete, Eventbrite::REST::Client::ENDPOINT + path)
+end
+
+def stub_get(path)
+  stub_request(:get, Eventbrite::REST::Client::ENDPOINT + path)
+end
+
+def stub_post(path)
+  stub_request(:post, Eventbrite::REST::Client::ENDPOINT + path)
+end
+
+def stub_put(path)
+  stub_request(:put, Eventbrite::REST::Client::ENDPOINT + path)
+end
+
+def fixture_path
+  File.expand_path('../fixtures', __FILE__)
+end
+
+def fixture(file)
+  File.new(fixture_path + '/' + file)
+end
