685d84848279a963f9d29825a605daeacde740ef
Steven Chau <steven.p.chau@gmail.com>
2014-04-23 12:45:19 -0700

    Spec .event_orders

--- spec/eventbrite/rest/events_spec.rb
+++ spec/eventbrite/rest/events_spec.rb
@@ -15,6 +15,19 @@ describe Eventbrite::REST::Events do
   end
 
   describe '.event_orders' do
+    subject { client.event_orders('123456789') }
+
+    before do
+      stub_get('/v3/events/123456789/orders/').
+        with(:query => {page:1}).
+        to_return(
+          :body => fixture('event_orders.json'),
+          :headers => {:content_type => 'application/json; charset=utf-8'}
+        )
+    end
+
+    it { subject; a_get('/v3/events/123456789/orders/').with(:query => {page:1}).should have_been_made }
+    it_behaves_like 'a cursor'
   end
 
   describe '.event_attendees' do
---
+++ spec/support/shared_examples.rb
@@ -0,0 +1,3 @@
+shared_examples 'a cursor' do
+  it { should be_a_kind_of(Eventbrite::Cursor) }
+end
