1ad10b070f48be1d4c23efdd676d6ca02dc6d1e4
Steven Chau <steven.p.chau@gmail.com>
2014-06-13 10:48:18 -0700

    Define Additional User Endpoints

--- lib/eventbrite/rest/users.rb
+++ lib/eventbrite/rest/users.rb
@@ -17,6 +17,22 @@ module Eventbrite
       def user_owned_events(user = nil, options = {})
         perform_with_cursor(:get, "/v3/users/#{extract_user_id(user)}/owned_events/", options, :events, Eventbrite::Event)
       end
+
+      def user_owned_event_orders(user = nil, options = {})
+        perform_with_cursor(:get, "/v3/users/#{extract_user_id(user)}/owned_event_orders/", options, :orders, Eventbrite::Order)
+      end
+
+      def user_owned_attendees(user = nil, options = {})
+        perform_with_cursor(:get, "/v3/users/#{extract_user_id(user)}/owned_event_attendees/", options, :attendees, Eventbrite::Attendee)
+      end
+
+      def user_venues(user = nil, options = {})
+        perform_with_cursor(:get, "/v3/users/#{extract_user_id(user)}/venues/", options, :venues, Eventbrite::Venue)
+      end
+
+      def user_organizers(user = nil, options = {})
+        perform_with_cursor(:get, "/v3/users/#{extract_user_id(user)}/organizers/", options, :organizers, Eventbrite::Organizer)
+      end
     end
   end
 end
