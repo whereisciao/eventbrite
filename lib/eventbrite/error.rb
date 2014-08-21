e66cb3b1c99acf70696205b14085b5125abf0079
Steven Chau <steven.p.chau@gmail.com>
2014-08-21 10:29:41 -0700

    Inspect show variables

--- lib/eventbrite/error.rb
+++ lib/eventbrite/error.rb
@@ -21,5 +21,11 @@ module Eventbrite
       @type = type
       @message = message
     end
+
+    def inspect
+      vars = self.instance_variables.
+        map{|v| "#{v}=#{instance_variable_get(v).inspect}"}.join(", ")
+      "<#{self.class}: #{vars}>"
+    end
   end
 end
