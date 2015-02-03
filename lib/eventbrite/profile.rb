ea705b83487476f9274e8e967e8b59320577f34a
Steven Chau <steven.p.chau@gmail.com>
2015-02-03 14:48:53 -0800

    Update Profile Fields

--- lib/eventbrite/profile.rb
+++ lib/eventbrite/profile.rb
@@ -2,7 +2,9 @@ require 'eventbrite/base'
 
 module Eventbrite
   class Profile < Eventbrite::Base
-    attr_reader :gender, :first_name, :last_name, :age, :email, :name
+    attr_reader :age, :birth_date, :blog, :cell_phone, :company,
+      :email, :first_name, :gender, :home_phone, :job_title,
+      :last_name, :name, :prefix, :suffix, :website, :work_phone
 
     object_attr_reader :Addresses, :addresses
   end
