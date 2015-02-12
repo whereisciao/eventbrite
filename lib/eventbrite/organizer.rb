feca6ebaf6e518a10aa3a7e6bfdee5fc8737bd40
Steven Chau <steven.p.chau@gmail.com>
2015-02-12 10:52:15 -0800

    Define accessor for Organizer Attrs.

--- lib/eventbrite/organizer.rb
+++ lib/eventbrite/organizer.rb
@@ -2,7 +2,9 @@ require 'eventbrite/identity'
 
 module Eventbrite
   class Organizer < Eventbrite::Identity
-    attr_reader :name, :url
+    attr_reader :name, :url, :resource_uri, :num_past_event, :num_future_events
     object_attr_reader :HtmlString, :description
+
+    def_delegator :description, :html, :description_html
   end
 end
