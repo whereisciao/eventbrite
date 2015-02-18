4b1e7baa74a9b8b5410ab0784d2b5cfc06eb5117
Steven Chau <steven.p.chau@gmail.com>
2015-02-18 13:38:07 -0800

    Expose Event Orders API

--- lib/eventbrite/rest/orders.rb
+++ lib/eventbrite/rest/orders.rb
@@ -6,6 +6,10 @@ module Eventbrite
     module Orders
       include Eventbrite::REST::Utils
 
+      def event_orders(event, options = {})
+        perform_with_cursor(:get, :v3, "/v3/events/#{extract_id(event)}/orders/", options, :orders, Eventbrite::Order)
+      end
+
       def order_details(order, options = {})
         perform_with_object(:get, :v3, "/v3/orders/#{extract_id(order)}/", options, Eventbrite::Order)
       end
