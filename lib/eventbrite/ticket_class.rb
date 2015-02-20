ceeab2dd1a1a9728e38881718ee1e764a5ed6fb4
Steven Chau <steven.p.chau@gmail.com>
2015-02-20 14:30:42 -0800

    Update TicketClass accessors

--- lib/eventbrite/ticket_class.rb
+++ lib/eventbrite/ticket_class.rb
@@ -2,11 +2,17 @@ require 'eventbrite/identity'
 
 module Eventbrite
   class TicketClass < Eventbrite::Identity
-    attr_reader :name, :free, :minimum_quantity,
-      :maximum_quantity, :quantity_total, :quantity_sold,
-      :description
+    attr_reader :resource_uri, :name, :description,
+      :donation, :free, :minimum_quantity,
+      :maximum_quantity, :quantity_total,
+      :quantity_sold, :hidden, :include_fee,
+      :split_fee, :hide_description, :auto_hide,
+      :variants, :event_id
+
     object_attr_reader :Money, :cost
     object_attr_reader :Money, :fee
+    object_attr_reader :Money, :actual_cost
+    object_attr_reader :Money, :actual_fee
 
     def sales_start
       Time.parse(@attrs[:sales_start]) unless @attrs[:sales_start].nil?
@@ -17,5 +23,13 @@ module Eventbrite
       Time.parse(@attrs[:sales_end]) unless @attrs[:sales_end].nil?
     end
     memoize :sales_end
+
+    def visible
+      !hidden
+    end
+
+    def quantity_available
+      @quantity_available ||= quantity_total - quantity_sold
+    end
   end
 end
