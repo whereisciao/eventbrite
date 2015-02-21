408b2ed3c1e981025c54c27bdd34f9a3c020148c
Steven Chau <steven.p.chau@gmail.com>
2015-02-20 17:07:04 -0800

    Venue use address reader

--- lib/eventbrite/venue.rb
+++ lib/eventbrite/venue.rb
@@ -3,6 +3,7 @@ require 'eventbrite/identity'
 module Eventbrite
   class Venue < Eventbrite::Identity
     attr_reader :name, :latitude, :longitude
-    object_attr_reader :Address, :address
+
+    address_reader :address
   end
 end
