41782bd2339696a78f9e875330e2e9610bc939d9
Steven Chau <steven.p.chau@gmail.com>
2015-03-16 12:58:32 -0700

    Syntax Sugar: User APIs uses a splat

--- lib/eventbrite/rest/users.rb
+++ lib/eventbrite/rest/users.rb
@@ -6,31 +6,39 @@ module Eventbrite
     module Users
       include Eventbrite::REST::Utils
 
-      def user_details(user = nil, options = {})
+      def user_details(*args)
+        user, options = parse_args(args)
         perform_with_object(:get, :v3, "/v3/users/#{extract_user_id(user)}/", options, Eventbrite::User)
       end
 
-      def user_orders(user = nil, options = {})
+      def user_orders(*args)
+        user, options = parse_args(args)
         perform_with_cursor(:get, :v3, "/v3/users/#{extract_user_id(user)}/orders/", options, :orders, Eventbrite::Order)
       end
 
-      def user_owned_events(user = nil, options = {})
+      def user_owned_events(*args)
+        user, options = parse_args(args)
+
         perform_with_cursor(:get, :v3, "/v3/users/#{extract_user_id(user)}/owned_events/", options, :events, Eventbrite::Event)
       end
 
-      def user_owned_event_orders(user = nil, options = {})
+      def user_owned_event_orders(*args)
+        user, options = parse_args(args)
         perform_with_cursor(:get, :v3, "/v3/users/#{extract_user_id(user)}/owned_event_orders/", options, :orders, Eventbrite::Order)
       end
 
-      def user_owned_attendees(user = nil, options = {})
+      def user_owned_attendees(*args)
+        user, options = parse_args(args)
         perform_with_cursor(:get, :v3, "/v3/users/#{extract_user_id(user)}/owned_event_attendees/", options, :attendees, Eventbrite::Attendee)
       end
 
-      def user_venues(user = nil, options = {})
+      def user_venues(*args)
+        user, options = parse_args(args)
         perform_with_cursor(:get, :v3, "/v3/users/#{extract_user_id(user)}/venues/", options, :venues, Eventbrite::Venue)
       end
 
-      def user_organizers(user = nil, options = {})
+      def user_organizers(*args)
+        user, options = parse_args(args)
         perform_with_cursor(:get, :v3, "/v3/users/#{extract_user_id(user)}/organizers/", options, :organizers, Eventbrite::Organizer)
       end
     end
