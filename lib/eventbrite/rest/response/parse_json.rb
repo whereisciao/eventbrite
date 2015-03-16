7955f44b91202f98ef086aef80f3ecc0359332f2
Steven Chau <steven.p.chau@gmail.com>
2015-03-16 12:36:40 -0700

    Parse JSON content type

--- lib/eventbrite/rest/response/parse_json.rb
+++ lib/eventbrite/rest/response/parse_json.rb
@@ -17,11 +17,19 @@ module Eventbrite
         end
 
         def on_complete(response)
-          response.body = parse(response.body) if respond_to?(:parse) && !unparsable_status_codes.include?(response.status)
+          if parseable?(response)
+            response.body = parse(response.body)
+          end
         end
 
         def unparsable_status_codes
           [204, 301, 302, 304]
+        end
+
+        def parseable?(response)
+          response.response_headers["Content-Type"] =~ /\bjson/ &&
+            respond_to?(:parse) &&
+            !unparsable_status_codes.include?(response.status)
         end
       end
     end
